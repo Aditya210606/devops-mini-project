b66fd5b49f87af35acba39a6904577392266c098
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-09-26 12:44:22 +0530

    changes in UI

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from DevOps! 🚀</h1><p>CI/CD Pipeline Working Successfully!</p>"
+    return """<h1>Hello from DevOps! 🚀</h1>
+              <p>CI/CD Pipeline Working Successfully!</p>
+              <p>Version 2 - Automatically Deployed 🚀</p>"""
 
 
 if __name__ == "__main__":
